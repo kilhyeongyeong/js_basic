2577b03e216d821c55a300cd15a1c337414eb86e
DESKTOP-SSBI791\bitcamp <kilhg0623@gmail.com>
2023-05-02 17:10:17 +0900

    JS 기초 2023.05.02

---
+++ 01_자바스크립트기초/02_산술연산자.js
@@ -0,0 +1,10 @@
+// 나머지 연산
+// console.log(27%5);
+
+// 거듭제곱 연산
+var num = 2;
+// console.log(num ** 5); // 2의 5승
+// console.log(num ** (1/2)); // 2의 1/2승
+
+// 2의 20승은 ~~~ 입니다.
+console.log(`2의 20승은 ${num ** 20}입니다.`);
